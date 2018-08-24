5f3361dd4e4307b7c55ec99620b92d427429e452
Geir Arne Hjelle <geirarne@gmail.com>
2018-08-24 22:51:41 +0200

    Simple GUI for Posetta

---
+++ posetta/gui/__init__.py
@@ -0,0 +1,8 @@
+"""GUI frontend for Posetta
+
+The GUI frontend gives an alternative for those that are not comfortable using
+the command line.
+
+"""
+
+from posetta.gui.posetta_gui import PosettaGui  # type: ignore  # noqa
